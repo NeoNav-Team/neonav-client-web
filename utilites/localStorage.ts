f513a5d55da3b43ee3fd27d48e0eab0d8eea8fa7
Michael LeMay <mjlemay@gmail.com>
2023-02-24 13:58:10 -0800

    attempt to fix phone laoding bug

--- utilites/localStorage.ts
+++ utilites/localStorage.ts
@@ -2,13 +2,15 @@ import isBrowser from './isBrowser';
 
 export const getLocalStorage = (key: string) => {
     const payload = localStorage.getItem(key) || '{}';
-    const decodedPayload =  JSON.parse(payload);
+    const stringState = payload.length >= 3 ? decodeURIComponent(escape(window.atob(payload))) : '{}';
+    const decodedPayload =  JSON.parse(stringState);
     return decodedPayload;
  }
 
 export const setLocalStorage = (key:string, payload:string) => {
     const encodedValue = JSON.stringify(payload);
-    localStorage.setItem(key, encodedValue);
+    const encodedStringState = window.btoa(unescape(encodeURIComponent(encodedValue)));
+    localStorage.setItem(key, encodedStringState);
  }
 
 export const clearLocalStorage = (key:string) => {
