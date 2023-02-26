20fdc7f9bfb3c704d6e148a3201b4fa749595574
Michael LeMay <mjlemay@gmail.com>
2023-02-26 15:10:21 -0800

    temp disables chat app for iphone testing

--- components/chatApp.tsx
+++ components/chatApp.tsx
@@ -1,14 +1,20 @@
 'use client';
-import React, { useCallback, useContext, useEffect, useState } from 'react';
+import React, { 
+  // useCallback, useContext, useEffect, useState 
+} from 'react';
 import { globalChannel } from '../utilites/constants';
-import styles from '../styles/generic.module.css';
-import { Container, Box, Stack } from '@mui/material';
-import InputChannelTab from './inputChannelTab';
-import ItemMessage from './itemMessage';
-import { Context as NnContext } from '../components/context/nnContext';
-import { NnChatMessage, NnProviderValues } from '../components/context/nnTypes';
-import SimpleScrollContainer from './simpleScrollContainer';
-import InputMessage from './inputMessage';
+// import styles from '../styles/generic.module.css';
+import { 
+  Container, 
+  // Box, 
+  // Stack,
+} from '@mui/material';
+// import InputChannelTab from './inputChannelTab';
+// import ItemMessage from './itemMessage';
+// import { Context as NnContext } from '../components/context/nnContext';
+// import { NnChatMessage, NnProviderValues } from '../components/context/nnTypes';
+// import SimpleScrollContainer from './simpleScrollContainer';
+// import InputMessage from './inputMessage';
 
 interface ChatAppProps {
   msgBtn?: boolean;
@@ -49,46 +55,46 @@ const flexFooter = {
 };
 
 export default function ChatApp(props:ChatAppProps):JSX.Element {
-  const { msgBtn } = props;
-  const { 
-    state,
-    fetchUserChannels = () => {},
-    fetchChannelHistory = (channelId:string) => {},
-    setSelected = (indexType:string, channelId:string) => {},
-  }: NnProviderValues = useContext(NnContext);
-  const selectedChannel:string = state.network?.selected?.channel || GLOBAL_CHAT;
-  const chatHistories = state.network?.collections?.chats;
-  const chatHistoriesIndex = chatHistories && chatHistories.map(function(x) {return x.id; }).indexOf(selectedChannel) || 0;
-  const [ channelsFetched, setChannelsFetched ] = useState<boolean>(false);
-  const [ chatsFetcedList, setChatsFetcedList ] = useState<string[]>([]);
-  const [ messages, setMessages ] = useState<NnChatMessage[]>([]);
-  const [ showMsgDrawer, setShowMsgDrawer ] = useState<boolean>(false);
-  const [ msg, setMsg ] = useState<string>('');
+  // const { msgBtn } = props;
+  // const { 
+  //   state,
+  //   fetchUserChannels = () => {},
+  //   fetchChannelHistory = (channelId:string) => {},
+  //   setSelected = (indexType:string, channelId:string) => {},
+  // }: NnProviderValues = useContext(NnContext);
+  // const selectedChannel:string = state.network?.selected?.channel || GLOBAL_CHAT;
+  // const chatHistories = state.network?.collections?.chats;
+  // const chatHistoriesIndex = chatHistories && chatHistories.map(function(x) {return x.id; }).indexOf(selectedChannel) || 0;
+  // const [ channelsFetched, setChannelsFetched ] = useState<boolean>(false);
+  // const [ chatsFetcedList, setChatsFetcedList ] = useState<string[]>([]);
+  // const [ messages, setMessages ] = useState<NnChatMessage[]>([]);
+  // const [ showMsgDrawer, setShowMsgDrawer ] = useState<boolean>(false);
+  // const [ msg, setMsg ] = useState<string>('');
 
-  const channelSelection = (selectedChannel:string) => {
-    setSelected('channel', selectedChannel);
-  }
+  // const channelSelection = (selectedChannel:string) => {
+  //   setSelected('channel', selectedChannel);
+  // }
 
-  const handleNext = () => {};
+  // const handleNext = () => {};
 
-  const goSendMessage = () => {
-    console.log('sending message');
-    setMsg('');
-  }
+  // const goSendMessage = () => {
+  //   console.log('sending message');
+  //   setMsg('');
+  // }
   
-  const goFetchChannels = useCallback(() => {
-    if (!channelsFetched) {
-      fetchUserChannels();
-      setChannelsFetched(true);
-    }
-  }, [channelsFetched, fetchUserChannels])
+  // const goFetchChannels = useCallback(() => {
+  //   if (!channelsFetched) {
+  //     fetchUserChannels();
+  //     setChannelsFetched(true);
+  //   }
+  // }, [channelsFetched, fetchUserChannels])
 
-  useEffect(() => {
-    const channels = state.user?.channels || []; 
-    if (channels.length === 0) {
-        goFetchChannels();
-    }
-  }, [state, goFetchChannels]);
+  // useEffect(() => {
+  //   const channels = state.user?.channels || []; 
+  //   if (channels.length === 0) {
+  //       goFetchChannels();
+  //   }
+  // }, [state, goFetchChannels]);
 
   // useEffect(() => {
   //   if (chatHistories && selectedChannel) {
@@ -117,7 +123,7 @@ export default function ChatApp(props:ChatAppProps):JSX.Element {
 
     return (
         <Container disableGutters style={{height: '100%'}}>
-             <div
+             {/* <div
                 className={styles.darkPane}
                 style={{height: '100%', maxHeight: 'calc(100% - 74px)', marginTop: '70px'}}
                 data-augmented-ui="tl-clip-x tr-clip-x br-clip bl-clip both"
@@ -147,7 +153,7 @@ export default function ChatApp(props:ChatAppProps):JSX.Element {
                   <InputMessage value={msg} clickHandler={() => setShowMsgDrawer(true)} />
                 </Box>
                 </Box>
-            </div>
+            </div> */}
         </Container>
     )
 }
