a394774db16b073fe99958a02d7bd5e866b27a14
Michael LeMay <mjlemay@gmail.com>
2023-02-26 19:13:12 -0800

    displays only chat message for debugging purposes

--- components/itemMessage.tsx
+++ components/itemMessage.tsx
@@ -4,6 +4,7 @@ import {
     Stack,
     Typography
 } from '@mui/material';
+import styles from '../styles/item.module.css';
 import { isoDateToDaily, isoDateToMonth, isoDateToOrbit } from '@/utilites/fomat';
 
 interface itemTransactionProps {
@@ -21,24 +22,24 @@ interface itemTransactionProps {
   
     return (
         <Box style={{padding: '1vh 0'}}>
-                <Stack direction="row" spacing={1} alignItems="flex-end">
+            {/* <Stack direction="row" spacing={1} alignItems="flex-end">
                 <Box sx={{minWidth:'40%', maxWidth: '40%'}}>
-                <div data-augmented-ui="tr-clip tr-rect both">
-                    <Typography>{id}</Typography>
-                    <Typography>{username}</Typography>
+                <div className={styles.nameLine} data-augmented-ui="tr-clip tr-rect both">
+                    <Typography className={styles.idText}>{id}</Typography>
+                    <Typography className={styles.userText}>{username}</Typography>
                 </div>
                 </Box>
                 <Box sx={{minWidth:'45%', maxWidth: '59%'}}>
-                <div data-augmented-ui="tr-clip both">
+                <div className={styles.dateLine} data-augmented-ui="tr-clip both">
                     <Stack direction="row" spacing={1}>
-                        <Typography>{isoDateToDaily(date)}</Typography>
-                        <Typography><span>✦ {isoDateToMonth(date)}</span></Typography>
-                        <Typography><span>❂ {isoDateToOrbit(date)}</span></Typography>
+                        <Typography className={styles.dateText}>{isoDateToDaily(date)}</Typography>
+                        <Typography className={styles.dateText}><span>✦ {isoDateToMonth(date)}</span></Typography>
+                        <Typography className={styles.dateText}><span>❂ {isoDateToOrbit(date)}</span></Typography>
                     </Stack>
                 </div>
                 </Box>
-            </Stack>
-            <div data-augmented-ui="tr-clip br-round bl-round inlay">
+            </Stack> */}
+            <div className={isSystemMsg(id, username) ? styles.systemLine : styles.transactionLine} data-augmented-ui="tr-clip br-round bl-round inlay">
                 <Box>
                     <Typography> 》 {text}</Typography>
                 </Box>
