1ca73ce8532b12a08a6cb78398cb31fc3c4f25c7
Alex Weber <mr.alexweber@gmail.com>
2024-03-10 16:17:47 -0700

    link contacts and garden pages (#21)
    * add prettier rc, update eslint, pin node version w/ volta
    
    * format constants
    
    * add link to contact page
    
    * display message when no statuses
    
    * add default image for entity in garden
    
    * remove unused code
    
    * link garden search to contacts page
    
    * change garden search link to contact page
    link garden page to contact page
    keep placeholder in subheader
    
    ---------
    
    Co-authored-by: Alex Weber <alex.weber@smartsheet.com>

--- components/subheaderEntity.tsx
+++ components/subheaderEntity.tsx
@@ -1,37 +1,58 @@
-import { 
-  Box,
-  Typography,
-  Avatar,
-  Stack,
-} from '@mui/material';
-import styles from '../styles/item.module.css';
-
+import { Box, Typography, Avatar, Stack } from "@mui/material";
+import styles from "../styles/item.module.css";
+import { PersonAdd } from "@mui/icons-material";
+import Link from "next/link";
+import { defaultImg } from "@/utilities/defaultImg";
 
 interface subheaderEntityProps {
+  id?: string;
   title?: string;
   cover?: string;
   photo?: string;
 }
 
-export default function SubheaderEntity(props:subheaderEntityProps):JSX.Element {
-  const { photo, cover, title} = props;
+export default function SubheaderEntity(
+  props: subheaderEntityProps
+): JSX.Element {
+  const { photo, cover, title, id } = props;
 
   return (
-    <div 
-      className={styles.subtitleLine}
-      data-augmented-ui="tr-clip both"
-    >
-      <Box sx={{background: cover}}> 
+    <div className={styles.subtitleLine} data-augmented-ui="tr-clip both">
+      <Box sx={{ background: cover }}>
         <Stack
           direction="row"
-          justifyContent="flex-start"
+          justifyContent="space-between"
           alignItems="center"
-          spacing={2}
         >
-          <Avatar src={photo} variant="rounded" sx={{width: {xs: 60, md: 100, xl: 150}, height: {xs: 60, md: 100, xl: 150}}} />
-          <Typography variant="h2" sx={{fontSize: {xs: 24, md: 32, xl: 64}}}>{title}</Typography>
+          <Stack
+            direction="row"
+            justifyContent="flex-start"
+            alignItems="center"
+            spacing={2}
+          >
+            <Avatar
+              src={photo ?? defaultImg}
+              variant="rounded"
+              sx={{
+                width: { xs: 60, md: 100, xl: 150 },
+                height: { xs: 60, md: 100, xl: 150 },
+              }}
+            />
+            <Typography
+              variant="h2"
+              sx={{ fontSize: { xs: 24, md: 32, xl: 64 } }}
+            >
+              {title}
+            </Typography>
+          </Stack>
+          {false && (
+            <Link href={`/contacts/${id}`}>
+              Placeholder: you can add an icon here.
+              <PersonAdd />
+            </Link>
+          )}
         </Stack>
       </Box>
     </div>
-  )
+  );
 }
