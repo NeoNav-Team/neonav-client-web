001aecfc7d66ac99f3890e8c09c90a38bda57f91
mjukis <swede@wccorp.org>
2026-03-30 22:43:27 -0700

    Fix for some Safari viewport issues

--- app/head.tsx
+++ app/head.tsx
@@ -2,7 +2,7 @@ export default function Head() {
   return (
     <>
       <title>N E O N A V</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta content="width=device-width, initial-scale=1, viewport-fit=cover" name="viewport" />
       <meta name="description" content="Neotropolis inbound" />
       <link rel="icon" href="/favicon.ico" />
     </>
