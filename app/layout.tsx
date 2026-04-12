2efd1e6e99864cc63f8318d12bd255a17a258ea3
mjukis <swede@wccorp.org>
2026-04-11 23:05:28 -0700

    Align with manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,12 +26,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="theme-color" content="#001000" />
+        <meta name="theme-color" content="#002566" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="NeoNav" />
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="/manifest.json" crossOrigin="use-credentials"/>
       </head>
       <body>
         <ThemeProvider theme={theme} >
